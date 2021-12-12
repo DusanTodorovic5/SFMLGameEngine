1af77fa75bfde6317a4f16e186ef46c560bc408e
Dušan Todorović <79877752+DusanTodorovic5@users.noreply.github.com>
2021-12-12 13:43:34 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "engine.hpp"
+#include "scenesCollection.hpp"
+int main(int argc, char** argv) {
+    if (argc > 1){
+        std::string debugMode = argv[1];
+        if (debugMode == "--debug"){
+            Engine::engine()->setDebugMode(true);
+        }
+    }
+
+    SceneCollection::loadScenes();
+
+    Engine::engine()->start();
+    
+    delete Engine::engine();
+    delete SceneManager::manager();
+    return 0;
+}
---
+++ scenesCollection.hpp
@@ -0,0 +1,9 @@
+#ifndef SCENES_COLLECTION_CLASS
+#define SCENES_COLLECTION_CLASS
+
+class SceneCollection{
+    public:
+        static void loadScenes();
+};
+
+#endif
